f9ff0f56c448d8766f039c5b933643ead69ccb54
Ross Crawford-d'Heureuse <ross@lawpal.com>
2014-02-06 11:38:46 +0100

    better version proposition

--- hello_sign/__init__.py
+++ hello_sign/__init__.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
-__version__ = "0.1.5"
+__version_info__ = (0, 1, 5)
+__version__ = '.'.join(__version_info__)
 
 import logging
 logger = logging.getLogger('django.request')

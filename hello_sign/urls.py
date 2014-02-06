e0785d043d67f675b09838420626b71ce1b663ed
Ross Crawford-d'Heureuse <ross@lawpal.com>
2014-02-06 10:39:17 +0100

    made csrfexempt

--- hello_sign/urls.py
+++ hello_sign/urls.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 from django.conf.urls import patterns, url, include
+from django.views.decorators.csrf import csrf_exempt
 
 from .views import HelloSignWebhookEventHandler
 
 
 urlpatterns = patterns('',
-    url(r'^hellosign/event/$', HelloSignWebhookEventHandler.as_view(), name='hellosign_webhook_event'),
+    url(r'^hellosign/event/$', csrf_exempt(HelloSignWebhookEventHandler.as_view()), name='hellosign_webhook_event'),
 )

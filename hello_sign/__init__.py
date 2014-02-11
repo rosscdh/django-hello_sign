07f9913d2f89296e2673ce1c28455001061c173a
Ross Crawford-d'Heureuse <ross@lawpal.com>
2014-02-11 14:35:56 +0100

    made exception ahve more info

--- hello_sign/__init__.py
+++ hello_sign/__init__.py
@@ -13,7 +13,7 @@ HELLOSIGN_API_KEY = getattr(settings, 'HELLOSIGN_API_KEY', None)
 HELLOSIGN_CLIENT_ID = getattr(settings, 'HELLOSIGN_CLIENT_ID', None)
 HELLOSIGN_CLIENT_SECRET = getattr(settings, 'HELLOSIGN_CLIENT_SECRET', None)
 
-assert HELLOSIGN_AUTHENTICATION is not None, 'You must define a settings.HELLOSIGN_AUTHENTICATION see: http://www.hellosign.com/api/reference#EventHashVerification'
+assert HELLOSIGN_AUTHENTICATION is not None, 'You must define a settings.HELLOSIGN_AUTHENTICATION see: http://www.hellosign.com/api/reference#EventHashVerification *NB* ensure you are logged in as your primary account'
 assert HELLOSIGN_API_KEY is not None, 'You must define a settings.HELLOSIGN_API_KEY see: http://www.hellosign.com/home/myAccount/current_tab/integrations'
 assert HELLOSIGN_CLIENT_ID is not None, 'You must define a settings.HELLOSIGN_CLIENT_ID see: http://www.hellosign.com/home/myAccount/current_tab/integrations'
 assert HELLOSIGN_CLIENT_SECRET is not None, 'You must define a settings.HELLOSIGN_CLIENT_SECRET see: http://www.hellosign.com/home/myAccount/current_tab/integrations'

42da92bf3cf10972661723409ff3f36fbe3d9340
Ross Crawford-d'Heureuse <ross@lawpal.com>
2014-03-29 05:42:44 +0100

    url namespace changes

--- setup.py
+++ setup.py
@@ -2,13 +2,6 @@
 import os
 from setuptools import setup
 
-# Utility function to read the README file.
-# Used for the long_description.  It's nice, because now 1) we have a top level
-# README file and 2) it's easier to type in the README file than to put a raw
-# string in below ...
-def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
-
 setup(
     name = "django-hello_sign",
     version = "0.1.5",
@@ -21,20 +14,5 @@ setup(
     packages=['hello_sign'],
     install_requires = [
         'hellosign',
-    ],
-    classifiers=[
-        'Development Status :: 1 - Alpha',
-        'Environment :: Web Environment',
-        'Framework :: Django',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2.7',
-        'Topic :: Internet :: WWW/HTTP',
-        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
-        'Topic :: Internet :: WWW/HTTP :: WSGI',
-        'Topic :: Software Development :: Libraries :: Application Frameworks',
-        'Topic :: Software Development :: Libraries :: Python Modules',
     ],
 )

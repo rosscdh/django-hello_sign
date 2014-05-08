fdb485165f4d00de77ed06f70e40394472637806
Ross Crawford-d'Heureuse <ross@lawpal.com>
2014-05-08 11:01:21 +0200

    version bump

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name = "django-hello_sign",
-    version = "0.1.7",
+    version = "0.1.8",
     author = "Ross Crawford-d'Heureuse",
     author_email = "ross@lawpal.com",
     description = ("Django app for integrating with HelloSign"),
